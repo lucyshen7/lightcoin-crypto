03ce8a9af3f44ec3ed1d80c044f7fdda927ec296
Lucy Shen <lucyshen77@gmail.com>
2021-12-04 01:13:08 +0000

    refactored commit and finished the features

--- index.js
+++ index.js
@@ -1,29 +1,82 @@
-let balance = 500.00;
+class Account {
 
-class Withdrawal {
-
-  constructor(amount) {
-    this.amount = amount;
+  constructor(username) {
+    this.username = username;
+    this.transactions = [];
   }
 
-  commit() {
-    balance -= this.amount;
+  get balance() {
+    // Calculate the balance using the transaction objects.
+    let balance = 0;
+    for (const obj in this.transactions) {
+      balance += this.transactions[obj].value;
+    }
+    return balance;
+  }
+
+  addTransaction(transaction) {
+    this.transactions.push(transaction);
   }
 
 }
+// abstract class
+class Transaction {
 
+  constructor(amount, account) {
+    this.amount = amount;
+    this.account = account;
+  }
 
+  // check if transaction allowed / validate withdrawals
 
+  commit() {
+    if (!this.isAllowed()) return false;    
+    // keep track of the time of the transaction
+    this.time = new Date();
+    // add the transaction to the account
+    this.account.addTransaction(this);
+    // this.account.balance += this.value;
+    // }    
+    return true;
+  }  
+}
 
-// DRIVER CODE BELOW
-// We use the code below to "drive" the application logic above and make sure it's working as expected
+class Withdrawal extends Transaction {
 
-t1 = new Withdrawal(50.25);
+  get value() {
+    return -this.amount;
+  }  
+  // it has access to this.account b/c of parent
+  isAllowed() {
+    return (this.account.balance - this.amount >= 0);
+  }
+}
+
+class Deposit extends Transaction {
+  
+  get value() {
+    return this.amount;
+  }
+  // deposits always allowed
+  isAllowed() {
+    return true;
+  }
+}
+
+// DRIVER CODE
+
+const myAccount = new Account("billybob");
+
+console.log("Starting balance", myAccount.balance)
+
+t1 = new Deposit(120.00, myAccount);
 t1.commit();
-console.log('Transaction 1:', t1);
 
-t2 = new Withdrawal(9.99);
+t2 = new Withdrawal(50.00, myAccount);
 t2.commit();
-console.log('Transaction 2:', t2);
 
-console.log('Balance:', balance);
+t3 = new Withdrawal(500, myAccount); // should not be validated.
+t3.commit();
+
+console.log('Ending Balance:', myAccount.balance);
+// console.log('Transactions:', myAccount.transactions);
